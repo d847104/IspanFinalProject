ca0237cf42853bf19b64406a890054f0f37fffb3
Angel <angelpclin@gmail.com>
2024-07-12 18:34:26 +0800

    修正 model 路徑 增加 application-test.properties 用於連線本機 localhost 資料庫

--- warashibe/src/main/java/com/ispan/warashibe/model/Messenger.java
+++ warashibe/src/main/java/com/ispan/warashibe/model/Messenger.java
@@ -1,4 +1,4 @@
-package com.ispan.model;
+package com.ispan.warashibe.model;
 
 import java.sql.Date;
 import org.springframework.format.annotation.DateTimeFormat;
--- warashibe/src/main/java/com/ispan/warashibe/model/ProductPayMethod.java
+++ warashibe/src/main/java/com/ispan/warashibe/model/ProductPayMethod.java
@@ -1,4 +1,4 @@
-package com.ispan.model;
+package com.ispan.warashibe.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
