1e62671e4c6a7196bd4c6a44a6809b7a746520dc
Alan <d847104@gmail.com>
2024-07-12 11:31:12 +0800

    7/12 專案架構建立

---
+++ warashibe/src/test/java/com/ispan/eeit184/WarashibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit184;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarashibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
