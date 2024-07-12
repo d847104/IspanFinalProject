9ee967888bb27fd24c4063e4fc89ff348d2e1d1f
Shan <shanlovechiachia@yahoo.com.tw>
2024-07-12 16:24:41 +0800

    調整專案名稱一致

---
+++ warashibe/src/main/java/com/ispan/model/PayMethod.java
@@ -0,0 +1,27 @@
+package com.ispan.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="PayMethod")
+public class PayMethod {
+
+	@Id
+	@Column(name="payMethodID")
+	private Integer payMethodID;
+	
+	
+	@Column(name="payMethod")
+	private String payMethod;
+	
+}
