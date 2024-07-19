8c6fd9f19f7fcc3b1cf576b690a160079dd78e75
Alan <d847104@gmail.com>
2024-07-19 17:04:27 +0800

    7/19 前端初始架構

---
+++ warashibeVue/src/router/router.js
@@ -0,0 +1,16 @@
+// src/router.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/secure/Login.vue';
+import Home from '../views/Home.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
