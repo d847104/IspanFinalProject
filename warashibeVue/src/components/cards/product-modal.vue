4683607788086163f8e32a542a1e09660484b223
Shan <shanlovechiachia@yahoo.com.tw>
2024-07-26 14:11:43 +0800

    怪裡怪氣不小心在DEV做了VUE

---
+++ warashibeVue/src/components/cards/product-modal.vue
@@ -0,0 +1,90 @@
+<template>
+    <!-- Modal -->
+    <div ref="exampleRef" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table>
+                        <tr>
+                            <td>Id : </td>
+                            <td><input type="text" name="id" :value="product.id"
+                                    @input="doinput('id', $event.target.value)"></td>
+                            <!-- <td><input type="text" name="id" :value="modelValue.id" @input="doinput('id',$event)"></td> -->
+                        </tr>
+                        <tr>
+                            <td>Name : </td>
+                            <td><input type="text" name="name" :value="product.name"
+                                    @input="doinput('name', $event.target.value)"></td>
+                        </tr>
+                        <tr>
+                            <td>Price : </td>
+                            <td><input type="text" name="price" :value="product.price"
+                                    @input="doinput('price', $event.target.value)"></td>
+                        </tr>
+                        <tr>
+                            <td>Make : </td>
+                            <td><input type="text" name="make" :value="product.make"
+                                    @input="doinput('make', $event.target.value)"></td>
+                        </tr>
+                        <tr>
+                            <td>Expire : </td>
+                            <td><input type="text" name="expire" :value="product.expire"
+                                    @input="doinput('expire', $event.target.value)"></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" v-show="isShowInsertButton"
+                        @click="emits('customInsert')">新增</button>
+                    <button type="button" class="btn btn-primary" v-show="!isShowInsertButton"
+                        @click="emits('customUpdate')">修改</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// function doinput(key,event){...
+function doinput(key, eventTargetValue) {
+    // emits("update:modelValue",{...
+    emits("update:product", {
+        ...props.product,
+        [key]: eventTargetValue
+    })
+}
+// const props = defineProps(["isShowInsertButton","modelValue"])
+// const emits = defineEmits(["update:modelValue"])
+const props = defineProps(["isShowInsertButton", "product"])
+const emits = defineEmits(["customInsert", "customUpdate", "update:product"])
+
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { ref, onMounted } from 'vue'
+
+const exampleRef = ref(null);
+const exampleModal = ref(null);
+
+onMounted(function () {
+    exampleModal.value = new bootstrap.Modal(exampleRef.value);
+});
+
+function showModal() {
+    exampleModal.value.show();
+}
+
+function hideModal() {
+    exampleModal.value.hide();
+}
+
+defineExpose({
+    showModal, hideModal,
+});
+</script>
+
+<style></style>
---
+++ warashibeVue/src/views/Home.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <nav class="nav flex-column w-2 p-3">
+                    <a class="nav-link" href="#">服飾</a>
+                    <a class="nav-link" href="#">圖書</a>
+                    <a class="nav-link" href="#">3C用品</a>
+                    <a class="nav-link" href="#">生活用品</a>
+                    <a class="nav-link" href="#">家電用品</a>
+                    <a class="nav-link" href="#">運動用品</a>
+                    <a class="nav-link" href="#">寵物用品</a>
+                    <a class="nav-link active" aria-current="page" href="#">嬰幼兒用品</a>
+                    <!-- <a class="nav-link disabled" aria-disabled="true">Disabled</a> -->
+                </nav>
+            </div>
+           
+            <div class="col-md-10">
+                <div class="container ">
+                    <br>
+                    <div class="text-left">
+                        <h4>熱門商品</h4>
+                    </div>
+                    <div class="card d-inline-flex p-2 mb-3 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯8807.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">【馬克杯】</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯21569.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯36344.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯8807.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯21569.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="container">
+                    <div class="text-left">
+                        <h4>推薦給您</h4>
+                    </div>
+                    <div class="card d-inline-flex p-2 mb-3 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯84089.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯88795.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                    <div class="card d-inline-flex p-2 mx-2" style="max-width: 225px;">
+                        <img src="/src/img/馬克杯27117.jpg" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">馬克杯</h5>
+                            <p class="card-text">「單色馬克杯」『單色馬克杯』【單色馬克杯】</p>
+                            <a href="#" class="btn btn-secondary btn-sm mx-1">商品介紹</a>
+                            <a href="#" class="btn btn-secondary btn-sm ">加入購物車</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
