ed4e029089101e76a538133d90afc0a5432c3021
Shan <shanlovechiachia@yahoo.com.tw>
2024-07-29 18:25:35 +0800

    增加首頁輪播、調整橫幅

--- warashibeVue/src/views/Home.vue
+++ warashibeVue/src/views/Home.vue
@@ -12,54 +12,75 @@
         <br>
         <h2 class="lh-lg">物換心儀</h2>
         <div class="row">
-            <compSecHandCard v-for="product in productsSecHand" :key="product.productID" :product="product"></compSecHandCard>
+            <compSecHandCard v-for="product in productsSecHand" :key="product.productID" :product="product">
+            </compSecHandCard>
         </div>
     </div>
 </template>
 
 <script setup>
-        import { ref, onMounted } from 'vue';
-        import compCard from '@/components/compCard.vue';
-        import compSecHandCard from '@/components/compSecHandCard.vue';
-        import callPopular from '@/plugins/product_popular.js';
-        import callSecondHand from '@/plugins/product_secondhand.js';
-        import callRandom from '@/plugins/product_random';
+import { ref, onMounted } from 'vue';
+import compCard from '@/components/compCard.vue';
+import compSecHandCard from '@/components/compSecHandCard.vue';
+import callPopular from '@/plugins/product_popular.js';
+import callSecondHand from '@/plugins/product_secondhand.js';
+import callRandom from '@/plugins/product_random';
 
-        const productsPopular = ref([]);
-        const startPopular = ref(0);
-        const rowsPopular = ref(8);
-        const currentPagePopular = ref(0);
-        const totalPagesPopular = ref(0);
-        const totalPopular = ref(0);
-        const lastPageRowsPopular = ref(0);
+const productsPopular = ref([]);
+const startPopular = ref(0);
+const rowsPopular = ref(8);
+const currentPagePopular = ref(0);
+const totalPagesPopular = ref(0);
+const totalPopular = ref(0);
+const lastPageRowsPopular = ref(0);
 
-        const productsRandom = ref([]);
-        const startRandom = ref(0);
-        const rowsRandom = ref(8);
-        const currentPageRandom = ref(0);
-        const totalPagesRandom = ref(0);
-        const totalRandom = ref(0);
-        const lastPageRowsRandom = ref(0);
+const productsRandom = ref([]);
+const startRandom = ref(0);
+const rowsRandom = ref(8);
+const currentPageRandom = ref(0);
+const totalPagesRandom = ref(0);
+const totalRandom = ref(0);
+const lastPageRowsRandom = ref(0);
 
-        const productsSecHand = ref([]);
-        const startSecHand = ref(0);
-        const rowsSecHand = ref(8);
-        const currentPageSecHand = ref(0);
-        const totalPagesSecHand = ref(0);
-        const totalSecHand = ref(0);
-        const lastPageRowsSecHand = ref(0);
-        
-        onMounted(function() {
-                callPopular(productsPopular,startPopular,rowsPopular,currentPagePopular,totalPagesPopular,totalPopular,lastPageRowsPopular);
-                callRandom(productsRandom,startRandom,rowsRandom,currentPageRandom,totalPagesRandom,totalRandom,lastPageRowsRandom);
-                callSecondHand(productsSecHand,startSecHand,rowsSecHand,currentPageSecHand,totalPagesSecHand,totalSecHand,lastPageRowsSecHand);
-        })
+const productsSecHand = ref([]);
+const startSecHand = ref(0);
+const rowsSecHand = ref(8);
+const currentPageSecHand = ref(0);
+const totalPagesSecHand = ref(0);
+const totalSecHand = ref(0);
+const lastPageRowsSecHand = ref(0);
+
+onMounted(function () {
+    callPopular(productsPopular, startPopular, rowsPopular, currentPagePopular, totalPagesPopular, totalPopular, lastPageRowsPopular);
+    callRandom(productsRandom, startRandom, rowsRandom, currentPageRandom, totalPagesRandom, totalRandom, lastPageRowsRandom);
+    callSecondHand(productsSecHand, startSecHand, rowsSecHand, currentPageSecHand, totalPagesSecHand, totalSecHand, lastPageRowsSecHand);
+})
 </script>
 
 <style scoped>
 .button-container {
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 10px; /* 可調整，根據你的設計 */
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
+    /* 可調整，根據你的設計 */
+}
+
+.container .row {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1.5rem;
+    /* 調整間距 */
+    margin-bottom: 1.5rem;
+    /* 可以根據需要進行調整 */
+}
+
+.container .row:last-of-type {
+    margin-bottom: 0;
+    /* 最後一行不需要額外間距 */
+}
+
+.lh-lg {
+    line-height: 1.5;
+    /* 可以根據需要進行調整 */
 }
 </style>
--- warashibeVue/src/views/Navigation.vue
+++ warashibeVue/src/views/Navigation.vue
@@ -1,96 +1,98 @@
 <template>
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <!-- 網站LOGO -->
+            <RouterLink class="navbar-brand" to="/">
+                <img src="@/assets/logo.png" alt="Logo" class="logo navbar-brand">
+            </RouterLink>
+
+            <!-- 漢堡選單按鈕 -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                <ul class="navbar-nav mb-2 mb-lg-0 w-100">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="#">商城/二手</RouterLink>
+                    </li>
+                    <!-- 搜尋列 -->
+                    <form class="d-flex search-form d-none d-lg-flex mx-auto" role="search">
+                        <input class="form-control me-1 search-bar" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <button class="btn btn-outline-success search-button" type="submit">
+                            <font-awesome-icon icon="fa-solid fa-search" />
+                        </button>
+                    </form>
+                    <!-- 右側功能選單 -->
+                    <li class="nav-item d-none d-lg-block">
+                        <RouterLink class="nav-link" to="#">
+                            <font-awesome-icon :icon="['fas', 'cart-shopping']" size="2x" />
+                        </RouterLink>
+                    </li>
+                    <li class="nav-item d-none d-lg-block">
+                        <RouterLink class="nav-link" to="#">
+                            <font-awesome-icon icon="fa-solid fa-comments" size="2x" />
+                        </RouterLink>
+                    </li>
+                    <li class="nav-item d-none d-lg-block">
+                        <RouterLink class="nav-link" :to="{ path: '/other/notification' }">
+                            <font-awesome-icon icon="fa-solid fa-bell" size="2x" />
+                        </RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/secure/login">登入/註冊</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" :to="{ path: '/secure/member' }">會員中心</RouterLink>
+                    </li>
+                    <!-- 會員相關 Dropdown List -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
-                            <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            會員相關
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/profile')">會員基本資料
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/buyer/buyerorder')">買家訂單
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller-orders')">賣家訂單
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#"
+                                    @click="checkAuth('/seller/sellermanageproduct')">賣家商品管理</RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/add-product')">上架商品
+                                </RouterLink>
+                            </li>
                         </ul>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <button class="btn btn-outline-dark" type="submit">
-                        <i class="bi-cart-fill me-1"></i>
-                        Cart
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                    </button>
-                </form>
             </div>
         </div>
-    </nav> -->
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top container">
-    <div class="container-fluid">
-        <!-- 網站LOGO -->
-        <RouterLink class="navbar-brand" to="/">
-            <img src="@/assets/logo.png" alt="Logo" class="logo navbar-brand">
-        </RouterLink>
-
-    <!-- 漢堡選單按鈕 -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mb-2 mb-lg-0 w-100">
-        <li class="nav-item">
-            <RouterLink class="nav-link" to="#">商城/二手</RouterLink>
-        </li>
-        <!-- 搜尋列 -->
-        <form class="d-flex search-form d-none d-lg-flex mx-auto" role="search">
-            <input class="form-control me-1 search-bar" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success search-button" type="submit">
-            <font-awesome-icon icon="fa-solid fa-search" />
-            </button>
-        </form>
-        <!-- 右側功能選單 -->
-        <li class="nav-item d-none d-lg-block">
-            <RouterLink class="nav-link" to="#">
-                <font-awesome-icon :icon="['fas', 'cart-shopping']" size="2x" />
-            </RouterLink>
-        </li>
-        <li class="nav-item d-none d-lg-block">
-            <RouterLink class="nav-link" to="#">
-            <font-awesome-icon icon="fa-solid fa-comments" size="2x" />
-            </RouterLink>
-        </li>
-        <li class="nav-item d-none d-lg-block">
-            <RouterLink class="nav-link" :to="{path:'/other/notification'}">
-            <font-awesome-icon icon="fa-solid fa-bell" size="2x" />
-            </RouterLink>
-        </li>
-        <li class="nav-item">
-            <RouterLink class="nav-link" to="/secure/login">登入/註冊</RouterLink>
-        </li>
-        <li class="nav-item">
-            <RouterLink class="nav-link" :to="{path:'/secure/member'}">會員中心</RouterLink>
-        </li>
-        <!-- 會員相關 Dropdown List -->
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            會員相關
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-            <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/profile')">會員基本資料</RouterLink></li>
-            <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/buyer/buyerorder')">買家訂單</RouterLink></li>
-            <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller-orders')">賣家訂單</RouterLink></li>
-            <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller/sellermanageproduct')">賣家商品管理</RouterLink></li>
-            <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/add-product')">上架商品</RouterLink></li>
-            </ul>
-        </li>
-        </ul>
+    </nav>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="2000">
+                <img src="/src/img/馬克杯21569.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="/src/img/馬克杯36344.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="/src/img/馬克杯84089.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
     </div>
-    </div>
-</nav>
-
 </template>
 
 <script setup>
@@ -112,52 +114,89 @@ const checkAuth = (path) => {
 
 <style scoped>
 .navbar {
-padding: 5px 15px;
-background-color: #343a40;
-border-bottom: 1px solid #e7e7e7;
-font-size: 1.5rem;
+    padding: 5px 15px;
+    background-color: #343a40;
+    /* 背景顏色透明度設為 0，表示完全透明 */
+    border-bottom: 1px solid rgba(231, 231, 231, 0.5);
+    /* 設置邊框顏色的透明度 */
+    font-size: 1.5rem;
+    z-index: 1000;
+    /* 確保 navbar 顯示在輪播上方 */
+    line-height: calc(50px * 0.8);
+    /* 調整行高以保持內容居中 */
 }
 
 .logo {
-width: 100px;
-height: auto;
+    width: 100px;
+    height: auto;
 }
 
 .nav-item {
-margin: 0 15px;
-text-decoration: none;
-color: #fff;
+    margin: 0 15px;
+    text-decoration: none;
+    color: #fff;
 }
 
 .nav-link {
-font-size: 1.5rem;
+    font-size: 1.5rem;
 }
 
 .search-form {
-width: 30%; /* 調整搜尋框寬度 */
-max-width: 1000px;
+    width: 30%;
+    /* 調整搜尋框寬度 */
+    max-width: 1000px;
 }
 
 .search-bar {
-width: 100%;
-padding: 10px;
-font-size: 1.5rem;
+    width: 100%;
+    padding: 10px;
+    font-size: 1.5rem;
 }
 
 .search-button {
-padding: 10px 15px;
-font-size: 1.5rem;
+    padding: 10px 15px;
+    font-size: 1.5rem;
 }
 
 .w-100 {
-display: flex;
-justify-content: space-between;
-align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
-@media (max-width: 992px) {
-.search-form {
-    display: none;
+.carousel {
+    width: 80vw;
+    /* 設定輪播寬度為螢幕寬度的80% */
+    height: 50vh;
+    /* 設定輪播高度為螢幕高度的50% */
+    margin: auto;
+    /* 使輪播居中 */
+    overflow: hidden;
+    /* 隱藏溢出內容 */
 }
+
+.carousel-inner {
+    height: 100%;
+    /* 使輪播內部容器高度與輪播相同 */
+    align-items: center;
+    justify-content: center;
+
+}
+
+.carousel-item {
+
+    /* 使用 flexbox 使圖片在輪播項目中居中 */
+    height: 100%;
+}
+
+
+.carousel-item img {
+    max-width: 100%;
+    max-height: 100%;
+    /* 確保圖片在容器內最大化，但不超過容器的大小 */
+    object-fit: cover;
+    /* 保持圖片比例，並填充容器 */
+    display: block;
+    /* 確保圖片是塊級元素 */
 }
 </style>
