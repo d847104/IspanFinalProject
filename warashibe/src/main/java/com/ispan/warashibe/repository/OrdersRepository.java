c5d63d8680ea84e5480d4cded93f325d140bec77
Angel <angelpclin@gmail.com>
2024-08-01 13:20:40 +0800

    (1) Favorite 增加以 memberID 查詢 (2) Orders 增加可以分頁查詢全部訂單

--- warashibe/src/main/java/com/ispan/warashibe/repository/OrdersRepository.java
+++ warashibe/src/main/java/com/ispan/warashibe/repository/OrdersRepository.java
@@ -2,6 +2,9 @@ package com.ispan.warashibe.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -14,5 +17,10 @@ public interface OrdersRepository extends JpaRepository<Orders, Integer>, JpaSpe
 	
 	default List<Orders> findBySellerId(Integer sellerId) {
 		return findAll((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("seller").get("memberID"), sellerId));
+	}
+	
+	default Page<Orders> findByPage(Pageable pgb) {
+		Specification<Orders> spec = (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
+		return findAll(spec, pgb);
 	}
 }
