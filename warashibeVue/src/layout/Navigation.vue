c73f700a937e27dc5b409557d0b066faaa944036
Shan <shanlovechiachia@yahoo.com.tw>
2024-08-02 18:39:27 +0800

    1.NAV改放大鏡樣式、HOME改輪播樣式但是還不能從資料庫抓圖出來 2.BUYERORDER還有訂單狀態功能、訂單明細按鈕未串接 3.會員基本資料還有照片顯示及修改的串接

--- warashibeVue/src/layout/Navigation.vue
+++ warashibeVue/src/layout/Navigation.vue
@@ -1,169 +1,191 @@
 <template>
     <!-- <div class="container"> -->
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
             <!-- 網站LOGO -->
             <RouterLink class="navbar-brand" to="/">
                 <img src="@/img/logo.png" alt="Logo" class="logo navbar-brand">
             </RouterLink>
             <!-- 漢堡選單按鈕 -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0 w-100">
-                <!-- 左側導航連結 -->
-                <li class="nav-item">
-                    <RouterLink class="nav-link" to="/">首頁</RouterLink>
-                </li>
-                <li class="nav-item">
-                    <RouterLink class="nav-link" to="#">商城/二手</RouterLink>
-                </li>
-                <!-- 搜尋列 -->
-                <form class="d-flex search-form d-none d-lg-flex mx-auto" role="search">
-                    <input class="form-control me-1 search-bar" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-secondary search-button" type="submit">
-                    <font-awesome-icon icon="fa-solid fa-search" />
-                    </button>
-                </form>
-                <!-- 右側功能選單 -->
-                <li class="nav-item d-none d-lg-block">
-                    <RouterLink class="nav-link" to="#">
-                    <font-awesome-icon icon="fa-solid fa-comments" />
-                    </RouterLink>
-                </li>
-                <li class="nav-item d-none d-lg-block" @mouseover="showPopup" @mouseleave="hidePopup">
-                    <NotificationPop :popupVisible="popupVisible" />
-                </li>
-                <li class="nav-item" v-if="!user">
-                    <RouterLink class="nav-link" to="/secure/login">登入</RouterLink>
-                </li>
-                <li class="nav-item" v-if="!user">
-                    <RouterLink class="nav-link" to="/secure/registerOne">註冊</RouterLink>
-                </li>
-                <li class="nav-item" v-if="user">
-                    <span class="nav-link">{{ user?.username }}</span>
-                </li>
-                <li class="nav-item" v-if="user">
-                    <button class="nav-link active" @click="logout">登出</button>
-                </li>
-                <li class="nav-item">
-                    <RouterLink class="nav-link" :to="{path:'/secure/member'}">會員中心</RouterLink>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    會員相關
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                        <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/profile')">會員基本資料</RouterLink></li>
-                        <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/buyer/buyerorder')">買家訂單</RouterLink></li>
-                        <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller-orders')">賣家訂單</RouterLink></li>
-                        <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller/sellermanageproduct')">賣家商品管理</RouterLink></li>
-                        <li><RouterLink class="dropdown-item" to="#" @click="checkAuth('/add-product')">上架商品</RouterLink></li>
-                    </ul>
-                </li>        
-                </ul>        
+                    <!-- 左側導航連結 -->
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/">首頁</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="#">商城/二手</RouterLink>
+                    </li>
+                    <!-- 搜尋列 -->
+                    <form class="d-flex search-form d-none d-lg-flex mx-auto" role="search">
+                        <input class="form-control me-1 search-bar" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <button class="btn btn-outline-secondary search-button" type="submit">
+                            <font-awesome-icon icon="fa-solid fa-search" />
+                        </button>
+                    </form>
+                    <!-- 右側功能選單 -->
+                    <li class="nav-item d-none d-lg-block">
+                        <RouterLink class="nav-link" to="#">
+                            <font-awesome-icon icon="fa-solid fa-comments" />
+                        </RouterLink>
+                    </li>
+                    <li class="nav-item d-none d-lg-block" @mouseover="showPopup" @mouseleave="hidePopup">
+                        <NotificationPop :popupVisible="popupVisible" />
+                    </li>
+                    <li class="nav-item" v-if="!user">
+                        <RouterLink class="nav-link" to="/secure/login">登入</RouterLink>
+                    </li>
+                    <li class="nav-item" v-if="!user">
+                        <RouterLink class="nav-link" to="/secure/registerOne">註冊</RouterLink>
+                    </li>
+                    <li class="nav-item" v-if="user">
+                        <span class="nav-link">{{ user?.username }}</span>
+                    </li>
+                    <li class="nav-item" v-if="user">
+                        <button class="nav-link active" @click="logout">登出</button>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" :to="{ path: '/secure/member' }">會員中心</RouterLink>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            會員相關
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                            <li>
+                                <RouterLink class="dropdown-item" :to="{ name: 'members-basicinformation-link' }">會員基本資料
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/buyer/buyerorder')">買家訂單
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/seller-orders')">賣家訂單
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#"
+                                    @click="checkAuth('/seller/sellermanageproduct')">賣家商品管理</RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="dropdown-item" to="#" @click="checkAuth('/add-product')">上架商品
+                                </RouterLink>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
-            </div>
-        </nav>
+        </div>
+    </nav>
     <!-- </div> -->
 </template>
-    
-    <script setup>
-    import { ref, inject } from 'vue'
-    import { useRouter } from 'vue-router'
-    import NotificationPop from '@/components/NotificationPop.vue';
-    import axiosapi from '@/plugins/axios';
-    
-    const router = useRouter();
-    
-    // 登入相關
-    const user = inject('user');
-    const setUser = inject('setUser');
-    function logout(){
-        axiosapi.defaults.headers.authorization = "";
-        sessionStorage.removeItem("memberID");
-        sessionStorage.removeItem("token");
-        setUser(false);
-        router.push("/secure/login");
+
+<script setup>
+import { ref, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import NotificationPop from '@/components/NotificationPop.vue';
+import axiosapi from '@/plugins/axios';
+
+const router = useRouter();
+
+// 登入相關
+const user = inject('user');
+const setUser = inject('setUser');
+function logout() {
+    axiosapi.defaults.headers.authorization = "";
+    sessionStorage.removeItem("memberID");
+    sessionStorage.removeItem("token");
+    setUser(false);
+    router.push("/secure/login");
+}
+
+const popupVisible = ref(false);
+
+const showPopup = () => {
+    console.log("show");
+    popupVisible.value = true;
+};
+
+const hidePopup = () => {
+    console.log("hide");
+    popupVisible.value = false;
+};
+
+const checkAuth = (path) => {
+    if (!user.value) {
+        router.push('/secure/login');
+    } else {
+        router.push(path);
     }
-    
-    const popupVisible = ref(false);
-    
-    const showPopup = () => {
-        console.log("show");
-        popupVisible.value = true;
-    };
-    
-    const hidePopup = () => {
-        console.log("hide");
-        popupVisible.value = false;
-    };
-    
-    const checkAuth = (path) => {
-        if (!user.value) {
-            router.push('/secure/login');
-        } else {
-            router.push(path);
-        }
-    }
-    </script>
-    
-    <style scoped>
-    .navbar {
+}
+</script>
+
+<style scoped>
+.navbar {
     padding: 5px 15px;
     background-color: #343a40;
     border-bottom: 1px solid #e7e7e7;
     font-size: 1.5rem;
-    }
-    
-    .logo {
+}
+
+.logo {
     width: 75px;
     height: auto;
-    }
-    
-    .nav-item {
+}
+
+.nav-item {
     margin: 0 5px;
     text-decoration: none;
     color: #fff;
-    }
-    
-    .nav-link {
+}
+
+.nav-link {
     font-size: 1.5rem;
-    }
-    
-    .search-form {
-    width: 30%; /* 調整搜尋框寬度 */
+}
+
+.search-form {
+    width: 30%;
+    /* 調整搜尋框寬度 */
     max-width: 1000px;
-    }
-    
-    .search-bar {
+}
+
+.search-bar {
     width: 100%;
     padding: 10px;
     font-size: 1.5rem;
-    }
-    
-    .search-button {
+}
+
+.search-button {
     padding: 10px 15px;
     font-size: 1.5rem;
-    }
-    
-    .w-100 {
+}
+
+.w-100 {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    }
-    
-    @media (max-width: 992px) {
+}
+
+@media (max-width: 992px) {
     .search-form {
         display: none;
     }
-    }
-    
-    .dropdown:hover > .dropdown-menu {display: block;}
-    .dropdown > .dropdown-toggle:active {
-            pointer-events: none;
-    }
-    </style>
-    
+}
+
+.dropdown:hover>.dropdown-menu {
+    display: block;
+}
+
+.dropdown>.dropdown-toggle:active {
+    pointer-events: none;
+}
+</style>
